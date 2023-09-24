f6a6fe47f07940eb0a3f8cbcae6dfff6d4d7adec
Luvwahraan <Luvwahraan+github@pikaed.org>
2023-09-24 09:36:01 +0200

    Started buttons and leds classes.

--- plugins_stuff.py
+++ plugins_stuff.py
@@ -168,3 +168,134 @@ class ConnectHandle():
             dprint('Closing socket')
             self._socket.close()
 
+
+
+
+
+
+colorMap = {
+      'off': 0b10000000,
+      'white-dim': 0b10010101,
+      'white-medium': 0b10101010,
+      'white-bright': 0b10111111,
+      'white': 0b10101010,
+      'red-dim': 0b10000001,
+      'red-medium': 0b10000010,
+      'red-bright': 0b10000011,
+      'red': 0b10000010,
+      'green-dim': 0b10000100,
+      'green-medium': 0b10001000,
+      'green-bright': 0b10001100,
+      'green': 0b10001000,
+      'blue-dim': 0b10010000,
+      'blue-medium': 0b10100000,
+      'blue-bright': 0b10110000,
+      'blue': 0b10100000,
+      'yellow-dim': 0b10000101,
+      'yellow-medium': 0b10001010,
+      'yellow-bright': 0b10001111,
+      'yellow': 0b10001010,
+      'magenta-dim': 0b10010001,
+      'magenta-medium': 0b10100010,
+      'magenta-bright': 0b10110011,
+      'magenta': 0b10100010,
+      'cyan-dim': 0b10010100,
+      'cyan-medium': 0b10101000,
+      'cyan-bright': 0b10111100,
+      'cyan': 0b10101000,
+      'orange': 0b10001011,
+      'salmon': 0b10011011,
+      'red-orange': 0b10000111,
+      'red-pink': 0b10010011,
+      'pink': 0b10100111,
+      'purple': 0b10110010,
+      'indigo': 0b10100001,
+      'light-blue': 0b10111010,
+      'lime-green': 0b10001110
+  }
+
+led_names = {
+        grip_leds: [ 'S1', 'S2', 'S3', 'S4', 'S5', 'H1', 'H2', 'H3', 'H4' ],
+        panel1_leds: [ 'B10', 'B11', 'B12', 'B7', 'B8', 'B9', 'B6', 'B4', 'B2', 'B5', 'B3', 'B1' ],
+        panel2_leds [
+            'B2', 'B1', 'B4', 'B3',
+            'GearUpNose', 'GearIndicator', 'GearUpLeft',
+            'GearDownLeft', 'GearDownNose', 'GearDownRight',
+            'GearUpRight',
+            'B10', 'B8', 'B6', 'B9', 'B7', 'B5' ]
+    }
+
+class Button():
+    def _validatedSide(side):
+        if side == 'left' or side == 'right':
+            return side
+        else:
+            raise Exception( "Side has to be 'left' or 'right', not '{v}'".format(v=side) )
+    
+    def _validatedType(btype):
+        if btype == 'toggle' or btype == 'hold' or btype == 'timed':
+            return btype
+        else:
+            raise Exception( "btype has to be: toggle, hold, timed. Not '{v}'".format(v=side) )
+        
+    
+    def addLED( led ):
+        if not isinstance(led, LED):
+            raise Exception( 'Bad type: ' + str(led) )
+        
+        self._led[ led.name ] = led
+    __init__(self, nb, side, btype='togle' description=''):
+        if ( is_integer( nb ) and nb > 0 ):
+            self.number = nb
+        else:
+            raise Exception( 'Button number has to be a positive integer; '{v}' is not.'.format(v=nb) )
+        
+        self.side = _validatedSide(side)
+        self.btype = _validatedType(btype)
+        
+        if description != '':
+            self.description = str(description)
+        
+        # Need a list, but LED has to be unique.
+        _led = {}
+    
+
+class LED():
+    def _validatedSide(side):
+        if side == 'left' or side == 'right':
+            return side
+        else:
+            raise Exception( "Side has to be 'left' or 'right', not '{v}'".format(v=side) )
+        
+    
+    def changeColor(self, colorMap[ self.color ]):
+        # Should throw an error if doesn't exist
+        self.color = colorMap[ colorName ]
+        
+        self.colorName = colorName
+        
+    
+    __init__(self, name, side, colorName, device, active=False):
+    """
+        name
+            see led_names list
+            
+        side
+            left or right
+            
+        colorName
+            see colorMap dict
+            
+        device
+            what device the led is on: master or slave
+            
+        active (optional)
+            True when LED on, else False
+    """
+        self.side = _validatedSide(side)
+        self.device = device
+        self.active = active
+        
+        changeColor(colorName)
+        
+    
